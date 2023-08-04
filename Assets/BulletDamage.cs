9ddc8b9dcd78cb3208acd20c61fc29eef49117f4
BrysonTaylorPortfolio <110854006+BrysonTaylorPortfolio@users.noreply.github.com>
2023-08-04 15:29:00 -0400

    FINALPROJECT
    DONE!!!

---
+++ Assets/BulletDamage.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    public Playerhealth playerhealth;
+    public int damage = 20;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            playerhealth.TakeDamage(damage);
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ Assets/Shooter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    
+    public EnemyAiTutorial enemyAiTutorial;
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            enemyAiTutorial.TakeDamage(200);
+        }
+    }
+
+}
+
